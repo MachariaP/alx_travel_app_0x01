6283cb574d8d302f5a4fb4ebbed9132264ed347c
Phinehas <walburphinehas78@gmail.com>
2025-11-29 11:27:45 +0300

    feat: configure main URLs with API routes and Swagger UI (/swagger/, /redoc/)

--- alx_travel_app/urls.py
+++ alx_travel_app/urls.py
@@ -14,30 +14,32 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path, include, re_path
+from django.urls import path, include
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
 schema_view = get_schema_view(
-        openapi.Info(
-            title="ALX Travel App API",
-            default_version='v1',
-            description="Travel listings platform",
-            contact=openapi.Contact(email="testdeveloperph@gmail.com"),
-            ),
-        public=True,
-        permission_classes=(permissions.AllowAny,),
-        )
+   openapi.Info(
+      title="ALX Travel App API",
+      default_version='v1',
+      description="API for managing listings and bookings",
+      contact=openapi.Contact(email="walburphinehas78@gmail.com"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    # Swagger UI
-    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    # API routes
+    path('api/', include('listings.urls')),
+    path('api/', include('bookings.urls')),
 
-    # Future API endpoints
-    path('api/', include('listings.urls')),  #placeholder
+    # Swagger UI
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
