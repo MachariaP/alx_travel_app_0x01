20e03c1ce8b09c49bd2200325ecb97c132f97fe5
Phinehas <walburphinehas78@gmail.com>
2025-11-29 11:27:45 +0300

    feat: create ListingSerializer for DRF

--- listings/serializers.py
+++ listings/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Listing, Booking
+from .models import Listing
 
 
 class ListingSerializer(serializers.ModelSerializer):
@@ -14,19 +14,3 @@ class ListingSerializer(serializers.ModelSerializer):
             'created_at', 'updated_at', 'host', 'host_id'
         ]
         read_only_fields = ['created_at', 'updated_at', 'host', 'host_id']
-
-
-class BookingSerializer(serializers.ModelSerializer):
-    listing = ListingSerializer(read_only=True)
-    listing_id = serializers.PrimaryKeyRelatedField(
-        queryset=Listing.objects.all(), source='listing', write_only=True
-    )
-    guest = serializers.ReadOnlyField(source='guest.username')
-
-    class Meta:
-        model = Booking
-        fields = [
-            'id', 'listing', 'listing_id', 'guest',
-            'check_in', 'check_out', 'total_price', 'created_at'
-        ]
-        read_only_fields = ['total_price', 'guest', 'created_at']
